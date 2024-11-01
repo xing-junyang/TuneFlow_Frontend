3f6d1946674e1d4781cb9a8cd399aa8cc6adff7b
xingjunyang <xingjunyang@smail.nju.edu.cn>
2024-11-01 10:45:58 +0800

    Fixed: Improved code quality.

--- src/api/userApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const login = async (credentials) => {
-  return axios.post('/api/users/login', credentials);
-};
-
-export const getUserInfo = async () => {
-  return axios.get('/api/users/me');
-};
